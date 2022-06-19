fff6ec5e63ec5e1e9780f66b0798581a17bbcf77
Daniel Porto <danieldsporto@outlook.com>
2022-06-19 17:41:33 -0300

    added filters to the header section

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
 import './App.css';
 import HQContainer from './components/HQContainer';
 import comicsList from './components/comicsList';
+import React from 'react';
+import comicList from './components/comicsList';
+import { ComicStatusEnum } from './model/ComicStatusEnum';
+import ComicType from './model/ComicType';
+
+type FilterType = {
+  toBuy : boolean;
+  unread : boolean;
+  read :  boolean;
+}
+
+const filteredComicList = (comicList: ComicType[], filters : FilterType) => {
+  return comicList.filter(x => (!filters.toBuy || x.status === ComicStatusEnum['A ser comprado']) && (!filters.unread || x.status === ComicStatusEnum['Não Lido']) && (!filters.read || x.status === ComicStatusEnum.Lido))
+}
 
 function App() {
+  const [filters, setFilters] = React.useState<FilterType>({toBuy: false, unread: false, read: false})
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>HQ Collection</h1>
+        <div className='button-box'>
+          <button className="button-style" onClick={()=>setFilters({...filters, toBuy: !filters.toBuy})}>A ser comprado</button>
+          <button className="button-style" onClick={()=>setFilters({...filters, unread: !filters.unread})}>Não Lido</button>
+          <button className="button-style" onClick={()=>setFilters({...filters, read: !filters.read})}>Lido</button>
+        </div>
       </header>
         <div className='content'>
-        <HQContainer comicsList={comicsList} />
+        <HQContainer comicsList={filteredComicList(comicList, filters)} />
         </div>
     </div>
   );
