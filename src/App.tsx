fbcf8902c272eefb4bf009da6864e86e241ef7c1
Daniel Porto <danieldsporto@outlook.com>
2022-06-20 01:20:46 -0300

    added statistics section

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 import comicList from './components/comicsList';
 import { ComicStatusEnum } from './model/ComicStatusEnum';
 import ComicType from './model/ComicType';
+import StatisticsContainer from './components/StatisticsContainer';
 
 type FilterType = {
   toBuy : boolean;
@@ -19,11 +20,15 @@ const filtersDisabledObject: FilterType = {
 }
 
 const filteredComicList = (comicList: ComicType[], filters : FilterType) => {
-  return comicList.filter(x => (!filters.toBuy || x.status === ComicStatusEnum['A ser comprado']) && (!filters.unread || x.status === ComicStatusEnum['Não Lido']) && (!filters.read || x.status === ComicStatusEnum.Lido))
+  return comicList.filter(x => 
+    (!filters.toBuy || x.status === ComicStatusEnum['A ser comprado']) 
+    && (!filters.unread || x.status === ComicStatusEnum['Não Lido']) 
+    && (!filters.read || x.status === ComicStatusEnum.Lido))
 }
 
 function App() {
   const [filters, setFilters] = React.useState<FilterType>(filtersDisabledObject)
+  const [graphsTabEnabled, setGraphsTabEnabled] = React.useState<boolean>(false)
 
   return (
     <div className="App">
@@ -33,10 +38,11 @@ function App() {
           <button className="button-style" onClick={()=>setFilters({...filtersDisabledObject, toBuy: !filters.toBuy})}>A ser comprado</button>
           <button className="button-style" onClick={()=>setFilters({...filtersDisabledObject, unread: !filters.unread})}>Não Lido</button>
           <button className="button-style" onClick={()=>setFilters({...filtersDisabledObject, read: !filters.read})}>Lido</button>
+          <button className="button-style" onClick={()=>setGraphsTabEnabled(!graphsTabEnabled)}>Estatísticas</button>
         </div>
       </header>
         <div className='content'>
-        <HQContainer comicsList={filteredComicList(comicList, filters)} />
+        { !graphsTabEnabled ? <HQContainer comicsList={filteredComicList(comicList, filters)} /> : <StatisticsContainer />}
         </div>
     </div>
   );
---
+++ src/components/StatisticsContainer.tsx
@@ -0,0 +1,51 @@
+import { Cell, Legend, Pie, PieChart, ResponsiveContainer, Tooltip } from "recharts";
+import { ComicStatusEnum } from "../model/ComicStatusEnum";
+import comicList from "./comicsList";
+
+type reducedComicsType = {
+    percentage: number;
+    status : string;
+};
+
+
+const COLORS = [
+    "#fb8500",
+    "#ffb703",
+    "#219ebc"
+];
+
+const renderColorfulLegendText = (value: string, entry: any) => {
+  const { color } = entry;
+
+  return <span style={{ color }}>{`${(parseFloat(value)*100).toFixed(2)}%`}</span>;
+};
+
+const StatisticsContainer = () => {
+
+    const dataArray: reducedComicsType[] = 
+    [
+        {percentage: (comicList.filter
+            (x => x.status === ComicStatusEnum["A ser comprado"]).length/comicList.length), 
+        status: ComicStatusEnum[0]},
+        {percentage: (comicList.filter
+            (x => x.status === ComicStatusEnum["Não Lido"]).length/comicList.length), 
+        status: ComicStatusEnum[1]},
+        {percentage: (comicList.filter
+            (x => x.status === ComicStatusEnum.Lido).length/comicList.length), 
+        status: ComicStatusEnum[2]},
+    ];
+
+    return <div className="pie-chart-container">
+    <ResponsiveContainer width={730} height="90%">
+        <PieChart  width={730} height={450}>
+            <Tooltip formatter={renderColorfulLegendText}/>
+            <Legend verticalAlign="top" height={0} />
+            <Pie data={dataArray} dataKey="percentage" nameKey="status" cx="50%" cy="50%" outerRadius={50} > 
+        	{dataArray.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>)} 
+            </Pie>
+        </PieChart>
+        </ResponsiveContainer>
+    </div>
+};
+
+export default StatisticsContainer;
