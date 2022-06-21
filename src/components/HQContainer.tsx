e996734393a009f98e88bef4750fd610fea7000d
Daniel Dias da Silva Porto <daniel.porto@radixeng.com.br>
2022-06-21 16:52:43 -0300

    Added Eaglemoss Collection number

--- src/components/HQContainer.tsx
+++ src/components/HQContainer.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import ComicType from '../model/ComicType';
-import HQElement from './HQElement';
+import React from "react";
+import ComicType from "../model/ComicType";
+import HQElement from "./HQElement";
 
 const style = {
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
 };
 
 type hqContainerProps = {
     comicsList: ComicType[];
 };
 
-const zeroPad = (num : number, places: number) => String(num).padStart(places, '0');
+const zeroPad = (num: number, places: number) =>
+    String(num).padStart(places, "0");
 
 const HQContainer = ({ comicsList }: hqContainerProps) => {
-    return <div style={style  as React.CSSProperties}>
-        {comicsList.map(comic => <HQElement key={comic.collectionNumber} asset={`dcgbr${zeroPad(comic.collectionNumber,3)}_br_1.webp`} title={comic.label} status={comic.status}/>)}
-    </div>
-}
+    return (
+        <div style={style as React.CSSProperties}>
+            {comicsList.map((comic) => (
+                <HQElement
+                    key={comic.collectionNumber}
+                    asset={`dcgbr${zeroPad(
+                        comic.collectionNumber,
+                        3
+                    )}_br_1.webp`}
+                    title={comic.label}
+                    status={comic.status}
+                    collectionNumber={comic.collectionNumber}
+                />
+            ))}
+        </div>
+    );
+};
 
-export default HQContainer
+export default HQContainer;
--- src/components/HQElement.tsx
+++ src/components/HQElement.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import useImage from '../hooks/useImage';
-import { ComicStatusEnum } from '../model/ComicStatusEnum';
+import React from "react";
+import useImage from "../hooks/useImage";
+import { ComicStatusEnum } from "../model/ComicStatusEnum";
 
 type HQElementProps = {
     asset: string;
     title: string;
     status: ComicStatusEnum;
-}
+    collectionNumber: number;
+};
 
 const wrapperDivStyle = {
     display: "flex",
@@ -16,14 +17,14 @@ const wrapperDivStyle = {
     margin: "1em",
     padding: "1em 0",
     borderRadius: "10px",
-    maxWidth: "350px"
+    maxWidth: "350px",
 };
 const infoDivStyle = {
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
     justifyContent: "space-evenly",
-    padding: "1em"
+    padding: "1em",
 };
 
 const statusDivStyle = {
@@ -31,43 +32,61 @@ const statusDivStyle = {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-evenly",
-    padding: "1em"
+    padding: "1em",
 };
 
-const statusColorSelection = (status : ComicStatusEnum) => {
-    let divColor : string;
-    switch(status) {
+const statusColorSelection = (status: ComicStatusEnum) => {
+    let divColor: string;
+    switch (status) {
         case 1:
             divColor = "#ffee93";
             break;
-            case 2:
-                divColor = "#a0ced9";
-                break;
-            default:
-                divColor = "#ffc09f";
-                break;
-        }
+        case 2:
+            divColor = "#a0ced9";
+            break;
+        default:
+            divColor = "#ffc09f";
+            break;
+    }
     return divColor;
-}
+};
 
-
-const HQElement = ({asset, title, status}: HQElementProps) => {
+const HQElement = ({
+    asset,
+    title,
+    status,
+    collectionNumber,
+}: HQElementProps) => {
     const { loading, error, image } = useImage(asset);
 
-    return (<div style={wrapperDivStyle as React.CSSProperties}>
-        <>
-            {/*@ts-expect-error*/}
-            {error && <b>{error.message}</b>}
-            {!error && loading ? <b>loading_image</b> : <img src={image} alt={`img_${title}`} width={"150px"}/>}
-            <div style={infoDivStyle  as React.CSSProperties}>
-                <b>{title}</b>
-                <div style={statusDivStyle  as React.CSSProperties}>
-                    <div style={{height: "20px", width: "20px", backgroundColor: statusColorSelection(status), marginRight: "1em"}} />
-                    {ComicStatusEnum[status]}
+    return (
+        <div style={wrapperDivStyle as React.CSSProperties}>
+            <>
+                {/*@ts-expect-error*/}
+                {error && <b>{error.message}</b>}
+                {!error && loading ? (
+                    <b>loading_image</b>
+                ) : (
+                    <img src={image} alt={`img_${title}`} width={"150px"} />
+                )}
+                <div style={infoDivStyle as React.CSSProperties}>
+                    <b>{title}</b>
+                    <div style={statusDivStyle as React.CSSProperties}>
+                        <div
+                            style={{
+                                height: "20px",
+                                width: "20px",
+                                backgroundColor: statusColorSelection(status),
+                                marginRight: "1em",
+                            }}
+                        />
+                        {ComicStatusEnum[status]}
+                    </div>
+                    <b>{`#${collectionNumber}`}</b>
                 </div>
-            </div>
-        </>
-    </div>)
-}
+            </>
+        </div>
+    );
+};
 
-export default HQElement
+export default HQElement;
